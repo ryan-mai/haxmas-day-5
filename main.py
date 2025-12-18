623488e75e600c2aebee45a973ba820c2afb550b
Ryan Mai <ryanmai757@gmail.com>
2025-12-18 04:51:37 +0000

    pls work render

--- main.py
+++ main.py
@@ -68,4 +68,5 @@ def receive_gifts():
     return flask.jsonify(gifts)
 
 if __name__ == "__main__":
-    app.run()
+    from os import environ
+    app.run(host='0.0.0.0', port=int(environ.get('PORT', 5000)))
